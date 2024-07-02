b45e98619a06c0017489c91e5282d8d082f2aff8
ChuhunovaElena <61413045+ChuhunovaElena@users.noreply.github.com>
2024-07-02 15:35:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const swiperTop = new Swiper('.top__swiper', {
+    // Optional parameters
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
+  var swiperAbout = new Swiper(".about__slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
