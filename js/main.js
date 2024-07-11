e97d5cd99bc4ffa9342e99a0939ca6ff55df530c
ChuhunovaElena <timofeeva17alena@yandex.ru>
2024-07-11 12:17:19 +0300

    comments added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const swiperTop = new Swiper('.top__swiper', {
     // Optional parameters
     effect: "fade",
-    //autoplay: {
-      //  delay: 2500,
-       // disableOnInteraction: false,
-     // },
+    autoplay: {
+       delay: 2500,
+       disableOnInteraction: false,
+     },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
